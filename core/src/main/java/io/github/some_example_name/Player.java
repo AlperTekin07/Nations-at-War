6f7a7193499cc6ae9c82c75790c7dca662ffc763
Amil Ibrahimli <ibrahimliamil9@gmail.com>
2026-04-01 18:32:46 +0300

    Troop and TroopManager
    Added placeholder Troop abstract class and TroopManager class for battlefield logic

--- core/src/main/java/io/github/some_example_name/Player.java
+++ core/src/main/java/io/github/some_example_name/Player.java
@@ -9,20 +9,17 @@ public class Player {
     public Player(int playerID){
         this.playerID = playerID;
         this.resources = 100; // placeholder
-        this.health = 1000; // placeholder (health of the base)
+        this.health = 1000; // placeholder
     }
 
-    // handles base damage
     public void takeBaseDamage(int damage){
         this.health = Math.max(this.health - damage, 0);
     }
 
-    // checks if the player's base is destroyed
     public boolean isDefeated(){
         return this.health <= 0;
     }
 
-    // checks if the player has enough sources
     public boolean spendResources(int cost){
         if(this.resources >= cost){
             this.resources -= cost;
@@ -36,15 +33,7 @@ public class Player {
     }
 
     // getters
-    public int getPlayerID(){
-        return this.playerID;
-    }
-
-    public int getHealth(){
-        return this.health;
-    }
-
-    public int getResources(){
-        return this.resources;
-    }
+    public int getPlayerID(){ return this.playerID; }
+    public int getHealth(){ return this.health; }
+    public int getResources(){ return this.resources; }
 }
